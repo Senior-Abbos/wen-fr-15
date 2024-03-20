8b282092c8cc1a57e644acd3ff56734a6b9dcf12
abbos-khamidov <khamidov3538@gmail.com>
2024-03-20 15:43:47 +0500

    added files from mr prepod

---
+++ lesson - 2/main.js
@@ -0,0 +1,15 @@
+// let num1 = 50;
+// let num2 = 20
+
+// console.log(num1+num2)
+// console.log(num1*num2)
+
+// // Вычислить сколько вам дней с даты вашего рождения с помощью js
+
+
+current_year = 2024;
+birth_year = 2009;
+years = current_year - birth_year;
+console.log('Вам', years, 'лет')
+console.log('Вам', years * 12, 'месяцев')
+console.log('Вам', years * 365, 'дней')
